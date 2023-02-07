5e6acc0d286a020eb396f6497800c3e02850dd17
kava <wabutrune.p@hotmail.com>
2023-02-07 16:43:29 +0100

    feat: easyAdmin DashboardController et UserCrudController effectué

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
